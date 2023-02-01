3e347622e096c944679d51e39fe5a827b6ce350c
Joelsct98 <joelsct98@gmail.com>
2023-02-01 16:09:28 +0100

    Master: hecho cambios para ver bien las imagenes de servicios y cambio de nombre en footer

--- Vue/template/footer.js
+++ Vue/template/footer.js
@@ -134,7 +134,7 @@ new Vue({
         </div>
         <div class="row">
           <div class="col text-center">
-            <span>Bayiva Web &copy; 2022 - All rights reserved</span>
+            <span>Energia y Salud Web &copy; 2022 - All rights reserved</span>
           </div>
         </div>
       </div>
@@ -170,7 +170,7 @@ new Vue({
                         </a>
                       </div>
                       <div class="col-4">
-                        <a id="icons" href="http://bayiva.com/.">
+                        <a id="icons" href="http://Energia y Salud.com/.">
                           <svg width="4em" height="4em" viewBox="0 0 16 16" class="bi bi-bootstrap-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4h-8zm1.06 12h3.475c1.804 0 2.888-.908 2.888-2.396 0-1.102-.761-1.916-1.904-2.034v-.1c.832-.14 1.482-.93 1.482-1.816 0-1.3-.955-2.11-2.542-2.11H5.062V12zm1.313-4.875V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375v2.725z"/>
                           </svg>
@@ -273,7 +273,7 @@ new Vue({
         </div>
         <div class="row">
           <div class="col text-center">
-            <span>Bayiva Web &copy; 2022 - All rights reserved</span>
+            <span>Energia y Salud Web &copy; 2022 - All rights reserved</span>
           </div>
         </div>
       </div>
@@ -305,7 +305,7 @@ new Vue({
                           </a>
                         </div>
                         <div class="col-4">
-                          <a  id="icons" href="http://bayiva.com/.">
+                          <a  id="icons" href="http://Energia y Salud.com/.">
                              <img src="img/tiktok.png"  width="30em" height="30em">
 
                           </a>
@@ -320,7 +320,7 @@ new Vue({
             <div class="col-6 col-md-3 col-lg-3 mb-2">
               <div class="row">
                 <div class="col-12 col-md-12 col-lg-12">
-                    <strong>Links</strong>
+                    <strong>Enlaces</strong>
                 </div>
                 <div class="col-12 col-md-12 col-lg-12">
                   <div class="row">
@@ -334,7 +334,7 @@ new Vue({
                       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                       </svg>
-                      <a   href="paginas/blog.html">Blog</a>
+                      <a   href="#Proyectos">Servicios</a>
                     </div>
                     <div class="col-12">
                       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -357,19 +357,19 @@ new Vue({
                       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                       </svg>
-                      <a   href="index.html">Desarrollador Web</a>
+                      <a   href="index.html">Pérdida de peso</a>
                     </div>
                     <div class="col-12">
                       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                       </svg>
-                      <a   href="index.html">Desarrollador Android</a>
+                      <a   href="index.html">Medicina tradicional china y acupuntura</a>
                     </div>
                     <div class="col-12">
                       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                       </svg>
-                      <a   href="index.html">Gestion Base de Datos</a>
+                      <a   href="index.html">Elaboración de dietas personalizadas</a>
                     </div>
                   </div>
                 </div>
@@ -386,19 +386,19 @@ new Vue({
                       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-envelope-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
                       </svg>
-                      <a   href="index.html#contacto">joelsct98@gmail.com</a>
+                      <a   href="index.html#contacto">vicjimper@gmail.com</a>
                     </div>
                     <div class="col-12">
                       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-telephone-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M2.267.98a1.636 1.636 0 0 1 2.448.152l1.681 2.162c.309.396.418.913.296 1.4l-.513 2.053a.636.636 0 0 0 .167.604L8.65 9.654a.636.636 0 0 0 .604.167l2.052-.513a1.636 1.636 0 0 1 1.401.296l2.162 1.681c.777.604.849 1.753.153 2.448l-.97.97c-.693.693-1.73.998-2.697.658a17.47 17.47 0 0 1-6.571-4.144A17.47 17.47 0 0 1 .639 4.646c-.34-.967-.035-2.004.658-2.698l.97-.969z"/>
                       </svg>
-                      <a   href="index.html#contacto">+34 685 21 71 94</a>
+                      <a   href="index.html#contacto">+34 647 041 788</a>
                     </div>
                     <div class="col-12">
                       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-signpost-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path d="M7 1.414V4h2V1.414a1 1 0 0 0-2 0zM1 5a1 1 0 0 1 1-1h10.532a1 1 0 0 1 .768.36l1.933 2.32a.5.5 0 0 1 0 .64L13.3 9.64a1 1 0 0 1-.768.36H2a1 1 0 0 1-1-1V5zm6 5h2v6H7v-6z"/>
                       </svg>
-                      <a   href="index.html#contacto">C/ Soledad Cazorla</a>
+                      <a   href="index.html#contacto">C/ Rueza 6, 28011 Madrid</a>
                     </div>
                   </div>
                 </div>
@@ -407,7 +407,7 @@ new Vue({
           </div>
           <div class="row">
             <div class="col text-center">
-              <span>Bayiva Web &copy; 2022 - All rights reserved</span>
+              <span>Energia y Salud Web &copy; 2022 - All rights reserved</span>
             </div>
           </div>
         </div>
--- Vue/template/navbar.js
+++ Vue/template/navbar.js
@@ -6,7 +6,7 @@ new Vue({
       <div class="container-fluid ">
           <a class="navbar-brand" href="../index.html">
             <img src="../img/medicina-tradicional.png" alt="logo-bayiba">
-            <strong style="font-size: 35px;">Bayiva</strong>.com
+            <strong style="font-size: 35px;">Energia y Salud</strong>.com
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -50,7 +50,7 @@ new Vue({
       <div class="container-fluid ">
           <a class="navbar-brand" href="..\\..\\index.html">
             <img src="../../img/medicina-tradicional.png" alt="logo-bayiba">
-            <strong>Bayiva</strong>.com
+            <strong>Energia y Salud</strong>.com
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>

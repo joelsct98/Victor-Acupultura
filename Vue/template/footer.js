b92c4193c147c7f2b93982dbeba7883c514d41d9
Joelsct98 <joelsct98@gmail.com>
2023-02-24 10:56:45 +0100

    Añadida imagenes en la pagina

--- Vue/template/footer.js
+++ Vue/template/footer.js
@@ -15,13 +15,13 @@ new Vue({
                   <div class="col-6">
                     <div class="row">
                       <div class="col-4">
-                        <a id="icons" href="https://github.com/joelsct98">
+                        <a id="icons" href="#">
                           <img src="../img/github1.png"  width="30em" height="30em">
 
                           </a>
                       </div>
                       <div class="col-4">
-                        <a  id="icons" href="https://www.linkedin.com/in/joel-chavez-b36b931b1/">
+                        <a  id="icons" href="#">
                           <svg xmlns="http://www.w3.org/2000/svg" width="4em" height="4em" viewBox="0 0 34 34" class="global-nav__logo">
                             <title>LinkedIn</title>
                                       <g>
@@ -154,13 +154,13 @@ new Vue({
                   <div class="col-6">
                     <div class="row">
                       <div class="col-4">
-                        <a id="icons" href="https://github.com/joelsct98">
+                        <a id="icons" href="#">
                           <img src="../../img/github1.png"  width="30em" height="30em">
 
                           </a>
                       </div>
                       <div class="col-4">
-                        <a id="icons" href="https://www.linkedin.com/in/joel-chavez-b36b931b1/">
+                        <a id="icons" href="#">
                           <svg xmlns="http://www.w3.org/2000/svg" width="4em" height="4em" viewBox="0 0 34 34" class="global-nav__logo">
                             <title>LinkedIn</title>
                                       <g>
@@ -170,7 +170,7 @@ new Vue({
                         </a>
                       </div>
                       <div class="col-4">
-                        <a id="icons" href="http://Energia y Salud.com/.">
+                        <a id="icons" href="#">
                           <svg width="4em" height="4em" viewBox="0 0 16 16" class="bi bi-bootstrap-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4h-8zm1.06 12h3.475c1.804 0 2.888-.908 2.888-2.396 0-1.102-.761-1.916-1.904-2.034v-.1c.832-.14 1.482-.93 1.482-1.816 0-1.3-.955-2.11-2.542-2.11H5.062V12zm1.313-4.875V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375v2.725z"/>
                           </svg>
@@ -293,19 +293,19 @@ new Vue({
                     <div class="col-6">
                       <div class="row">
                         <div class="col-4">
-                          <a  id="icons" href="https://github.com/joelsct98">
+                          <a  id="icons" href="#">
                             <img src="img/youtube.png"  width="30em" height="30em">
 
                             </a>
                         </div>
                         <div class="col-4">
-                          <a  id="icons" href="https://www.linkedin.com/in/joel-chavez-b36b931b1/">
+                          <a  id="icons" href="#">
                              <img src="img/instagram.png"  width="30em" height="30em">
 
                           </a>
                         </div>
                         <div class="col-4">
-                          <a  id="icons" href="http://Energia y Salud.com/.">
+                          <a  id="icons" href="#">
                              <img src="img/tiktok.png"  width="30em" height="30em">
 
                           </a>
